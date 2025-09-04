65ca71b1653630a0192c264fc180b739a14a554c
Bryce Roethel <me@bryceroethel.com>
2025-09-03 20:08:49 -0400

    Update sitemap.xml

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,29 +1,33 @@
+const ROOT = `https://team340.org`;
+
 export async function GET() {
-	return new Response(
-		`
-		<?xml version="1.0" encoding="UTF-8" ?>
-		<urlset
-			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-			xmlns:xhtml="https://www.w3.org/1999/xhtml"
-			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-		>
-            <url>
-                <loc>https://team340.org/</loc>
-                <priority>1.0</priority>
-            </url>
-            <url><loc>https://team340.org/about</loc></url>
-            <url><loc>https://team340.org/robots</loc></url>
-            <url><loc>https://team340.org/resources</loc></url>
-            <url><loc>https://team340.org/sponsors</loc></url>
-            <url><loc>https://team340.org/contact</loc></url>
-		</urlset>`.trim(),
-		{
-			headers: {
-				'Content-Type': 'application/xml'
-			}
-		}
-	);
+	const body = sitemap([``, `/about`, `/robots`, `/resources`, `/sponsors`, `/contact`]);
+	const response = new Response(body);
+
+	response.headers.set('Content-Type', 'application/xml');
+	response.headers.set('Cache-Control', 'max-age=0, s-maxage=3600');
+
+	return response;
 }
+
+const sitemap = (pages: string[]) => `<?xml version="1.0" encoding="UTF-8" ?>
+<urlset
+    xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+    xmlns:xhtml="https://www.w3.org/1999/xhtml"
+    xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+    xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+    xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+>
+    ${pages
+			.map(
+				(page) => `
+    <url>
+        <loc>${ROOT}/${page}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.5</priority>
+    </url>
+    `
+			)
+			.join('')}
+</urlset>`;
