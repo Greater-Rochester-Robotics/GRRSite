6a2419d45294f149cb728fd6388e9e73d88f1e97
Bryce Roethel <me@bryceroethel.com>
2026-01-11 10:48:40 -0500

    Update homepage countdown

--- src/lib/data/nextEvent.ts
+++ src/lib/data/nextEvent.ts
@@ -8,10 +8,10 @@ export interface NextEvent {
 }
 
 export const nextEvent: NextEvent = {
-	name: `2026 FRC Season Kickoff`,
-	date: new Date(1768064400000),
+	name: `Finger Lakes Regional`,
+	date: new Date(1773266400000),
 	link: {
-		label: `FIRST Website`,
-		url: `https://www.firstinspires.org/programs/frc/game-and-season`
+		label: `Event Information`,
+		url: `https://www.thebluealliance.com/event/2026nyro`
 	}
 };
